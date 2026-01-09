390f81224ba77976d3ae5014add70733ea813912
soranigo <micro765@gmail.com>
2026-01-10 00:43:44 +0900

    fix:rubocopのlintエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 16 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8, maximum: 32}
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }
 end
