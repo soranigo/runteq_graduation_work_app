fb14abad78d91f102e1755176bdd0b9bae4ef00d
soranigo <micro765@gmail.com>
2026-01-18 22:25:17 +0900

    fix:デプロイ環境でのメール送信機能を停止。Renderの無料プランでは、APIを使用しないとできず、卒業予定日までの実装は厳しいと判断したため

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
 
   # === SendGridを使ったメール送信設定 ===
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {
     host: ENV.fetch("APP_HOST", "example.com"),
     protocol: "https"
