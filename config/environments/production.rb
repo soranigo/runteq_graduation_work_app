23bf71a2da98334e674fd6c075a357ab54f3a4c6
soranigo <micro765@gmail.com>
2026-01-18 20:26:21 +0900

    fix:デプロイ環境でのメール送信機能を改修。SendGridを用いたメール送信にした。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,26 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  # === SendGridを使ったメール送信設定 ===
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { 
+    host: ENV.fetch('APP_HOST', 'localhost:3000'),
+    protocol: 'https'
+  }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: ENV['APP_HOST'],
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
