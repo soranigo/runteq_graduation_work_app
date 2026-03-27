e1c41aa382229d5c157ae1fc7f55c823cde63bfc
soranigo <micro765@gmail.com>
2026-03-27 19:11:13 +0900

    fix:brakemanのバージョン指定と、importmapの更新

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "application"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.20
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.100
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.100
