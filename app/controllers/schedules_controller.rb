4636a3a14ea5b4068c1fe17dd2b242345e2a34aa
soranigo <micro765@gmail.com>
2026-04-01 20:19:31 +0900

    fix:スケジュール表示画面で、30分越えのプランを2つ以上の枠を使用して表示する(同じプラン名を表示する。ただし日を跨ぐプランには非対応)

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -23,7 +23,16 @@ class SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find(params[:id])
     @plans = @schedule.plans.all
-    @plans_hash = @plans.index_by { |plan| [ plan.starting_day_of_week_before_type_cast, plan.starting_time_before_type_conversion.strftime("%H:%M") ] }
+    @plans_hash = @plans.index_by { |plan| [
+                                              plan.starting_day_of_week_before_type_cast,
+                                              plan.starting_time,
+                                            ] }
+    @plan_ids_hash = {}
+    @plans_hash.each do |plan_hash|
+      (((plan_hash[1].ending_time_before_type_conversion - plan_hash[1].starting_time_before_type_conversion) / 1800).round - 1).times do |t|
+        @plan_ids_hash[ [ plan_hash[1].starting_day_of_week_before_type_cast, (plan_hash[1].starting_time_before_type_conversion + (t + 1) * 30 * 60).strftime("%H:%M") ] ] = plan_hash[1]
+      end
+    end
   end
 
   def notifications
@@ -46,5 +55,11 @@ class SchedulesController < ApplicationController
     if @schedule.user != current_user
       redirect_to schedules_path, notice: "他のユーザーへの干渉は許しません。"
     end
+  end
+
+  def time_slot_number_calculation(plan)
+    ending_time = plan.ending_day_of_week * 24 * 60 * 60 + plan.ending_time_before_type_conversion
+    starting_time = plan.starting_day_of_week * 24 * 60 * 60 + plan.starting_time_before_type_conversion
+    return ending_time - starting_time
   end
 end
